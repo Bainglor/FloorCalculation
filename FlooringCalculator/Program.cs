8e88153ee079b8e3e3043cd8d4a0066270e236ea
James Magee <JamesBMagee@outlook.com>
2024-02-14 08:20:00 +0000

    Updated with try catch and to run 9x3.

--- FlooringCalculator/Program.cs
+++ FlooringCalculator/Program.cs
@@ -8,36 +8,44 @@ namespace FlooringCalculator
 {
     /// <summary>
     /// Create nessecary dependencies and execute calculation.
+    /// f(x) = floorLayoutCalculator.GetFloorLayoutCount(9, 3)
     /// </summary>
-    
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            // create a logger
-            Log.Logger = new LoggerConfiguration()
-                            //.WriteTo.Console()
-                            .WriteTo.File("logs\\FlooringCalcualtor.txt",
-                                          restrictedToMinimumLevel: LogEventLevel.Information,
-                                          rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true)
-                            .MinimumLevel.Debug()
-                            .CreateLogger();
+            try {
+                // create a logger
+                Log.Logger = new LoggerConfiguration()
+                                //.WriteTo.Console()
+                                .WriteTo.File("logs\\FlooringCalcualtor.txt",
+                                              restrictedToMinimumLevel: LogEventLevel.Information,
+                                              rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true)
+                                .MinimumLevel.Debug()
+                                .CreateLogger();
 
 
 
-            List<int> boards = new List<int>();
-            boards.Add(2);
-            boards.Add(3);
+                List<int> boards = new List<int>();
+                boards.Add(2);
+                boards.Add(3);
 
-            //Should add the Microsoft DI package and add the dependencies for Logging
-            //and PermutationCalculator to it.
-            PermutationCalculator permutationCalculator = new PermutationCalculator(Log.Logger);
-            FloorLayoutCalculator floorLayoutCalculator = new FloorLayoutCalculator(boards, Log.Logger, permutationCalculator);
-            var floorLayoutCount = floorLayoutCalculator.GetFloorLayoutCount(32, 10);
+                //Should add the Microsoft DI package and add the dependencies for Logging
+                //and PermutationCalculator to it.
+                PermutationCalculator permutationCalculator = new PermutationCalculator(Log.Logger);
+                FloorLayoutCalculator floorLayoutCalculator = new FloorLayoutCalculator(boards, Log.Logger, permutationCalculator);
+                var floorLayoutCount = floorLayoutCalculator.GetFloorLayoutCount(9, 3);
 
-            var message = "Total Floor Layouts ";
-            Log.Logger.Information(message + floorLayoutCount);
-            Console.WriteLine(message + floorLayoutCount.ToString());
+                var message = "Total Floor Layouts ";
+                Log.Logger.Information(message + floorLayoutCount);
+                Console.WriteLine(message + floorLayoutCount.ToString());
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Error(ex.Message);
+                Console.WriteLine("No Calculation Performed, Error Encountered.");
+            }
             Console.ReadLine();
         }
 
